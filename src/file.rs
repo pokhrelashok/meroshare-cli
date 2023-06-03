2ebadfc8d1a5780af3887224a58932ad59c9d667
pokhrelashok2 <pokhrelashok2@gmail.com>
2023-06-03 09:12:57 +0545

    Token stored in temporary directory

--- src/file.rs
+++ src/file.rs
@@ -1,25 +1,32 @@
+use crate::user::User;
+use lazy_static::lazy_static;
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
 use std::{
     collections::HashMap,
     fs::{self, File, OpenOptions},
     io::{Read, Write},
 };
+use tempfile::NamedTempFile;
 
-use serde::{Deserialize, Serialize};
-
-use crate::user::User;
-
+lazy_static! {
+    static ref FILE_PATH: PathBuf = {
+        let file = NamedTempFile::new().expect("Failed to create temporary file");
+        file.path().to_owned()
+    };
+}
 pub fn create_file() {
     let mut file = OpenOptions::new()
         .create_new(true)
         .write(true)
         .append(true)
-        .open("tokens.txt")
+        .open(&*FILE_PATH)
         .unwrap();
     file.write_all(b"").unwrap();
 }
 
 pub fn delete_file() {
-    let file_path = "tokens.txt";
+    let file_path = &*FILE_PATH;
     match fs::remove_file(file_path) {
         Ok(()) => println!("File deleted successfully."),
         Err(err) => println!("Error deleting file: {}", err),
@@ -35,13 +42,13 @@ fn save_map_to_file(map: &HashMap<String, String>) -> Result<(), Box<dyn std::er
         .write(true)
         .create(true)
         .truncate(true)
-        .open("tokens.txt")?;
+        .open(&*FILE_PATH)?;
     file.write_all(json.as_bytes())?;
     Ok(())
 }
 
 fn read_map_from_file() -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
-    let mut file = File::open("tokens.txt")?;
+    let mut file = File::open(&*FILE_PATH)?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     let data: UserTokens = serde_json::from_str(&contents)?;
